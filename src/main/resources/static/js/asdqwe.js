42571e63e77341886e98e13950ca44e5eb50bc19
chepaskov <chepaksov@mail.ru>
2020-02-12 19:18:40 +0300

    1

---
+++ src/main/resources/static/js/asdqwe.js
@@ -0,0 +1,23 @@
+function asdqwe() {
+
+    $.ajax({
+
+        url: '/admin/alcoves',
+        datatype: 'json',
+        type: "post",
+        contentType: "application/json",
+        success: function (data) {
+
+            for (let item of data) {
+                $("#border").append("<div class='form-check-inline'>" +
+                    "<label class='font-weight-bold form-check-label' >" +
+                    "<input type='checkbox' class='form-check-input' id = '" + item.id+"' value = '" + item.role+"'>" +
+
+                    "                                " + item.role+"        </label>" +
+                    "                                    </div>");
+
+            }
+        }
+    });
+
+}
