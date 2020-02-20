8e36b1a14afa51850961715b884005deda563dde
chepaskov <chepaksov@mail.ru>
2020-02-20 13:11:00 +0300

    1

--- src/main/java/com/project/model/LocaleString.java
+++ src/main/java/com/project/model/LocaleString.java
@@ -15,30 +15,37 @@ public class LocaleString {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
     private long id;
 
 
-    @Column(name = "ru", nullable = false)
+    @Column(name = "ru")
+    @NonNull
     private String ru;
 
 
-    @Column(name = "en", nullable = false)
+    @Column(name = "en")
+    @NonNull
     private String en;
 
 
-    @Column(name = "fr", nullable = false)
+    @Column(name = "fr")
+    @NonNull
     private String fr;
 
 
-    @Column(name = "it", nullable = false)
+    @Column(name = "it")
+    @NonNull
     private String it;
 
 
-    @Column(name = "de", nullable = false)
+    @Column(name = "de")
+    @NonNull
     private String de;
 
 
-    @Column(name = "cs", nullable = false)
+    @Column(name = "cs")
+    @NonNull
     private String cs;
 
 
