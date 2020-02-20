461455c353b0efcc2d41a2b06791131dbe4f1826
chepaskov <chepaksov@mail.ru>
2020-02-20 13:34:31 +0300

    морда

--- src/main/java/com/project/config/handler/LoginSuccessHandler.java
+++ src/main/java/com/project/config/handler/LoginSuccessHandler.java
@@ -19,16 +19,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
                                         HttpServletResponse httpServletResponse,
                                         Authentication authentication) throws IOException, ServletException {
 
-        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
-//        if (roles.contains("admin")) {
-//            httpServletResponse.sendRedirect("/admin");
-//        }
-//
-//        else if (roles.contains("user")) {
-//            httpServletResponse.sendRedirect("/user");
-//        }
-//        else httpServletResponse.sendRedirect("/login");
 
     }
 }
