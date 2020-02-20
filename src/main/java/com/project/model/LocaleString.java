ed48f7a08693ac110fc0ed80c6c3894b92679b62
chepaskov <chepaksov@mail.ru>
2020-02-20 13:16:03 +0300

    удалил @nullable

--- src/main/java/com/project/model/LocaleString.java
+++ src/main/java/com/project/model/LocaleString.java
@@ -15,37 +15,30 @@ public class LocaleString {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @NonNull
     private long id;
 
 
     @Column(name = "ru")
-    @NonNull
     private String ru;
 
 
     @Column(name = "en")
-    @NonNull
     private String en;
 
 
     @Column(name = "fr")
-    @NonNull
     private String fr;
 
 
     @Column(name = "it")
-    @NonNull
     private String it;
 
 
     @Column(name = "de")
-    @NonNull
     private String de;
 
 
     @Column(name = "cs")
-    @NonNull
     private String cs;
 
 
