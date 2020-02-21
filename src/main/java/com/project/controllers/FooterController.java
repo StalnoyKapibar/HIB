d9f6d1c9654367e2489ed2ff864687f212180f1e
mchikunov <udafov@yandex.ru>
2020-02-21 21:05:18 +0300

    footer

--- src/main/java/com/project/controllers/FooterController.java
+++ src/main/java/com/project/controllers/FooterController.java
@@ -1,6 +1,8 @@
 package com.project.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,9 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
     @Controller
     public class FooterController {
 
-        @GetMapping("/footer")
-        public String getInternationalPage() {
+
+        @Value("${welcome.message}")
+        private String message;
+
+        @GetMapping("/")
+        public String main(Model model) {
+            model.addAttribute("message", message);
+
+
             return "footer";
         }
     }
-
