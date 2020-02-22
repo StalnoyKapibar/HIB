b99cbcbb862d0b6a3d7ec87f1b7c68ee5cb62598
mchikunov <udafov@yandex.ru>
2020-02-22 22:13:21 +0300

    footer1

--- src/main/java/com/project/springConfig/webConfig.java
+++ src/main/java/com/project/springConfig/webConfig.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -28,6 +29,8 @@ public class webConfig implements WebMvcConfigurer {
         lci.setParamName("lang");
         return lci;
     }
+
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
