0e59beb21662fa1aa158d7c61681db0d6ea87a99
chepaskov <chepaksov@mail.ru>
2020-02-25 16:16:26 +0300

    исправил

--- src/main/java/com/project/config/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.project.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
