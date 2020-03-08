e41a2fb468d0dfb5953f65ab4ed7a0e9107c4e6e
mchikunov <udafov@yandex.ru>
2020-03-08 08:19:13 +0300

    page added

---
+++ src/main/resources/static/js/page.js
@@ -0,0 +1,111 @@
+var currentLang = '';
+
+$(document).ready(function () {
+    if (currentLang == '') {
+        currentLang = $('#dd_menu_link').data('currentLang');
+    }
+    getLanguage();
+    setLocaleFields();
+});
+
+function setLocaleFields() {
+    fetch("/properties/" + currentLang)
+        .then(status)
+        .then(json)
+        .then(function (localeFields) {
+            $('#link_main_footer').text(localeFields['main1']);
+            $('#link_instruction').text(localeFields['instruction']);
+            $('#link_authors').text(localeFields['authors']);
+            $('#link_order').text(localeFields['order']);
+            $('#link_contacts').text(localeFields['contacts']);
+            $('#links').text(localeFields['links']);
+            $('#made_by').text(localeFields['madeby']);
+            $('#link_main_header').text(localeFields['main']);
+            $('#link_books_header').text(localeFields['books']);
+            $('#menu-toggle').text(localeFields['category']);
+        })
+}
+
+$('#dd_menu').on('click', 'a', function (eventOnInnerTag) {
+    eventOnInnerTag.preventDefault();
+    const selectedLang = $(eventOnInnerTag.target).attr('id');
+    fetch("/lang/" + selectedLang)
+        .then(status)
+        .then(text)
+        .then(function (data) {
+            currentLang = selectedLang;
+            window.location.replace('home?LANG=' + currentLang);
+            //TODO some logic to processing data and reload page with chosen lang
+            getLanguage();
+            getLocaleFields();
+        });
+    location.reload();
+});
+
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+function getLanguage() {
+    function getFullNameOfLanguage(language) {
+        switch (language) {
+            case 'ru' :
+                return 'Русский';
+            case 'en' :
+                return 'English';
+            case 'de' :
+                return 'Deutsch';
+            case 'it' :
+                return 'Italiano';
+            case 'fr' :
+                return 'Français';
+            case 'cs' :
+                return 'Český';
+        }
+        return "undef";
+    }
+
+    fetch("/lang")
+        .then(status)
+        .then(json)
+        .then(function (listOfLanguage) {
+            var currentLangFull = '';
+            var html = '';
+
+            for (language in listOfLanguage) {
+                if (currentLang == (listOfLanguage[language])) {
+                    continue;
+                }
+                currentLangFull = getFullNameOfLanguage(listOfLanguage[language]);
+                html += `<a class="dropdown-item lang" id="${listOfLanguage[language]}">
+                            <img src="../static/icons/${listOfLanguage[language]}.png" 
+                                alt="" height="16" width="16" class="lang-image"> - ${currentLangFull}
+                         </a>`;
+            }
+            $('#dd_menu').html(html);
+            $('#dd_menu_link').text(currentLang);
+            $('#dd_menu_link').empty();
+            $('#dd_menu_link').append(`<img src="../static/icons/${currentLang}.png"
+                                alt="" height="16" width="16" class="lang-image">`);
+        })
+}
+
+function status(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response)
+    } else {
+        return Promise.reject(new Error(response.statusText))
+    }
+}
+
+function json(response) {
+    return response.json()
+}
+
+function text(response) {
+    return response.text()
+}
+
+
+
