9128c3bfd8a6d9a2e782ad895da08cca69d864de
mchikunov <udafov@yandex.ru>
2020-03-14 18:11:42 +0300

    dfg

--- src/main/java/com/project/controller/restcontroller/BookController.java
+++ src/main/java/com/project/controller/restcontroller/BookController.java
@@ -5,13 +5,19 @@ import com.project.model.BookDTO20;
 import com.project.model.BookNewDTO;
 import com.project.service.BookService;
 import lombok.AllArgsConstructor;
+import lombok.var;
+import org.hibernate.engine.jdbc.StreamUtils;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 @AllArgsConstructor
 @RestController
@@ -66,6 +72,18 @@ public class BookController {
         BookDTO bookDTO = bookService.getBookByIdLocale(x);
         return ResponseEntity.ok(bookDTO);
     }
+
+    @RequestMapping(value = "/sid", method = RequestMethod.GET,
+            produces = MediaType.IMAGE_JPEG_VALUE)
+
+    public void getImage(HttpServletResponse response) throws IOException {
+
+        var imgFile = new Resource("img/tmp/qwe.jpg");
+
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+    }
+
 
 
     }
