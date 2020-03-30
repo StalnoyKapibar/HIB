7ba6eff5fca702d7352e757dc7e3160725790820
Alexander Podchasov <Kurodzuru@gmail.com>
2020-03-30 13:54:39 +0300

    переделал на fetch

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -9,28 +9,45 @@ $(document).ready(function () {
     setPageFields();
 });
 
-function setPageFields() {
-    $.ajax({
-        url: "/searchResult",
-        method: 'GET',
-        data: {
-            request: $("#request").attr("value"),
-            LANG: currentLang
-        }
-    }).then(function (data) {
-        let tr = [];
-        for (let i = 0; i < data.length; i++) {
-            tr.push('<tr>');
-            tr.push('<td class="align-middle"><img src="../images/book' + data[i].id + '/' + data[i].coverImage + '" style="max-width: 60px">');
-            tr.push('<td class="align-middle">' + data[i].nameAuthorDTOLocale + '</td>>');
-            tr.push('<td class="align-middle">' + data[i].nameBookDTOLocale + '</td>>');
-            tr.push('<td class="align-middle"><form id="bookButton" method="get" action="/page/' + data[i].id + '">' +
-                '<button class="btn btn-primary pageOfBook" id="buttonBookPage" name="bookPage">A page of book</button></form></td>>');
-            tr.push('</tr>');
-        }
-        $('table').append($(tr.join('')));
-    });
+function status(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response)
+    } else {
+        return Promise.reject(new Error(response.statusText))
+    }
 }
 
+function json(response) {
+    return response.json()
+}
 
+function text(response) {
+    return response.text()
+}
+
+function setPageFields() {
+    let req = $("#request").attr("value");
+    fetch("/searchResult?request=" + req + "&LANG=" + currentLang, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+        .then(status)
+        .then(json)
+        .then(function (data) {
+            let tr = [];
+            for (let i = 0; i < data.length; i++) {
+                tr.push('<tr>');
+                tr.push('<td class="align-middle"><img src="../images/book' + data[i].id + '/' + data[i].coverImage + '" style="max-width: 60px">');
+                tr.push('<td class="align-middle">' + data[i].nameAuthorDTOLocale + '</td>>');
+                tr.push('<td class="align-middle">' + data[i].nameBookDTOLocale + '</td>>');
+                tr.push('<td class="align-middle"><form id="bookButton" method="get" action="/page/' + data[i].id + '">' +
+                    '<button class="btn btn-primary pageOfBook" id="buttonBookPage" name="bookPage">A page of book</button></form></td>>');
+                tr.push('</tr>');
+            }
+            $('table').append($(tr.join('')));
+        });
+}
 
