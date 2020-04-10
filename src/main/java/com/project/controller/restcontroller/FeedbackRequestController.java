37e52daed7d98e880e76e7e3b6bfc7ec6e2ee522
Corexis <avdonin.001@gmail.com>
2020-04-10 14:17:51 +0300

    Убрана версия api, убран @PreAuthorize

--- src/main/java/com/project/controller/restcontroller/FeedbackRequestController.java
+++ src/main/java/com/project/controller/restcontroller/FeedbackRequestController.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 
@@ -25,7 +24,7 @@ public class FeedbackRequestController {
     private final Environment env;
 
     @PostMapping
-    @RequestMapping("/api/v1/feedback-request")
+    @RequestMapping("/api/feedback-request")
     public FeedbackRequest sendNewFeedBackRequest(@RequestBody FeedbackRequest feedbackRequest) {
         LOGGER.debug("POST request '/feedback-request' with {}", feedbackRequest);
         feedbackRequest.setId(null);
@@ -37,8 +36,7 @@ public class FeedbackRequestController {
     }
 
     @SuppressWarnings("all")
-    @PostMapping("/api/v1/admin/feedback-request/reply/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/api/admin/feedback-request/reply/{id}")
     public void send(@PathVariable Long id, @RequestBody SimpleMailMessage simpleMailMessage) {
         LOGGER.debug("POST request '/feedback-request/reply/{}' with {}", id, simpleMailMessage);
         FeedbackRequest feedbackRequest = feedbackRequestService.getById(id);
@@ -49,20 +47,17 @@ public class FeedbackRequestController {
         mailService.sendEmail(simpleMailMessage);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping(value = "/api/v1/admin/feedback-request", params = "!replied")
+    @GetMapping(value = "/api/admin/feedback-request", params = "!replied")
     public List<FeedbackRequest> getAll() {
         return feedbackRequestService.findAll();
     }
 
-    @GetMapping("/api/v1/admin/feedback-request")
-    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/api/admin/feedback-request")
     public List<FeedbackRequest> getByReplied(@RequestParam Boolean replied) {
         return feedbackRequestService.getByReplied(replied);
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping("/api/v1/admin/feedback-request/{id}")
+    @GetMapping("/api/admin/feedback-request/{id}")
     public FeedbackRequest getById(@PathVariable Long id) {
         return feedbackRequestService.getById(id);
     }
