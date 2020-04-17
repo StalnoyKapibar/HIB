eb3e26b1c32782906d8fff57b66c5974270b5cae
wakeupne0 <a.davidoff@outlook.com>
2020-04-17 19:25:55 +0300

    добавлено предзаполнение имени и почты для залогиненных юзеров на форму contact us

--- src/main/resources/static/js/contactForm.js
+++ src/main/resources/static/js/contactForm.js
@@ -1,9 +1,37 @@
+let userData;
 let hiddenButton = $('#hidden-submit-btn-feedback');
 let senderNameInput = $("#sender-name");
 let senderEmailInput = $("#sender-email");
 let senderMessageInput = $("#sender-message");
 let feedbackModal = $("#feedback-modal");
+let senderName = $('#sender-name');
+let senderEmail = $('#sender-email');
 const messageTemplate = "Hello, I m interested in the book ";
+
+$(document).ready(getUserData());
+
+function getUserData() {
+    fetch("/cabinet/getAU")
+        .then(status)
+        .then(json)
+        .then(function (resp) {
+            userData = resp;
+            senderEmail.val(userData.email);
+            senderName.val(userData.firstName + " " + userData.lastName);
+        });
+}
+
+function status(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response)
+    } else {
+        return Promise.reject(new Error(response.statusText))
+    }
+}
+
+function json(response) {
+    return response.json()
+}
 
 $(document).on('click', '#send-feedback-request', () => {
     hiddenButton.click();
