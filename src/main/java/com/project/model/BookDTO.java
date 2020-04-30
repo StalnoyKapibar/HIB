1f6983a547e47d44517479efb214df9f4f2a40e4
dagi.stogov@gmail.com <dagi.stogov@gmail.com>
2020-04-30 15:51:44 +0300

    end

--- src/main/java/com/project/model/BookDTO.java
+++ src/main/java/com/project/model/BookDTO.java
@@ -23,6 +23,21 @@ public class BookDTO {
     private String coverImage;
     @Singular("imageList")
     private List<Image> imageList;
-    private boolean isShow = true;
+    private boolean isShow;
     private Category category;
+
+    public BookDTO(long id, LocaleString name, LocaleString author, LocaleString desc, LocaleString edition, String yearOfEdition, Long pages, Long price, String originalLanguage, String coverImage, List<Image> imageList, boolean isShow) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.desc = desc;
+        this.edition = edition;
+        this.yearOfEdition = yearOfEdition;
+        this.pages = pages;
+        this.price = price;
+        this.originalLanguage = originalLanguage;
+        this.coverImage = coverImage;
+        this.imageList = imageList;
+        this.isShow = isShow;
+    }
 }
