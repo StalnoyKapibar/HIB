98b013fc435335c6d13856dd2c0aa2a288bf9207
endddddd <endddddd1@gmail.com>
2020-05-12 18:54:58 +0300

    editWelcome потрошитель

--- src/main/resources/static/js/admin/editWelcome.js
+++ src/main/resources/static/js/admin/editWelcome.js
@@ -41,40 +41,7 @@ async function getAllLocales() {
             nameVarOfLocaleStringWithId = tmp;
             nameVarOfLocaleStringWithId.unshift("id");
             nameVarOfLocaleString = nameVarOfLocaleStringWithId.filter(t => t !== "id");
-
-            let html = `<input id="welcome-id" type="hidden" value="${welcomeText[0]}"/>`;
-            for (let i = 0; i < nameVarOfLocaleString.length; i++) {
-                let tmp_html = nameVarOfLocaleString[i];
-                html += `<label for = ${tmp_html}>${tmp_html}</label>` +
-                    `<textarea type='text' class='form-control' id='${tmp_html}' ` +
-                    `aria-describedby='emailHelp'>${welcomeText[i + 1]}</textarea>`;
-            }
-            $('#form_id').html(html + `<button type='submit' onclick='funcStart()' class='btn btn-primary'>Submit</button>`);
         })
-}
-
-function funcStart() {
-    var aniArgs = {};
-    for (let tmp_html of tmp) {
-        aniArgs[tmp_html] = $('#' + tmp_html).val();
-    }
-    aniArgs['id'] = welcomeText[0];
-    var body02 = JSON.stringify({
-        id: 1,
-        body: aniArgs
-    });
-    updateWelcome(body02);
-}
-
-async function updateWelcome(x) {
-    await fetch("/api/admin/welcome/edit", {
-        method: 'POST',
-        body: x,
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    });
 }
 
 function status(response) {
