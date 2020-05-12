d92287acac10c0a1e06c799c0b4acd5ec6dd8531
orlovleo12 <orlovleo12>
2020-05-11 22:41:10 -0500

    Editpage new design.+ Page work.

--- src/main/resources/static/js/admin/buildEditPage.js
+++ src/main/resources/static/js/admin/buildEditPage.js
@@ -1,14 +1,17 @@
 let idd;
+let divAvatar;
+let listImages;
 let nameObjectOfLocaleString;
 let nameVarOfLocaleString;
 let tmpArr;
 let nameVarOfLocaleStringWithId;
-let pathImageDefault = 'images/book';
 let pathImageFin;
 let pathImageFinWithoutImage;
 let nameImage;
-let listImages;
 let nameImageCover = '';
+let disabledCheckBox = $('#disabled');
+let pathImageDefault = '../images/book';
+
 
 $(document).ready(getVarBookDTO());
 
@@ -48,7 +51,8 @@ function addPartsOfBook(partsOfBook) {
                 </div> </div>
                 <div class="row my-2">
                 <div class="col-0" for=${tmpNameObject}>${tmpNameObject} transliterate&nbsp;&nbsp; </div>
-                <div class="col-5 pl-5 ml-5  mr-1 "><input type='text' class='form-control ' id='in${tmpNameObject}'
+                <div class="col-5 pl-5 ml-5  mr-1 ">
+                <input type='text' class='form-control ' id='in${tmpNameObject}'
                 placeholder='${tmpNameObject} transliterate '>
                 </div> </div>
                 </div>
@@ -95,11 +99,14 @@ function buildPage() {
               <h4 >Book sold</h4></div>
               <div class="col"> <input id="disabled" class="big-checkbox"  type="checkbox" ${disabled}></div></div></div>`;
 
-    $('#bookEditPage').html(`<div class="tab-content" id="myTabContent">
-            
-            <div class="tab-pane fade show active" id="name" role="tabpanel" aria-labelledby="name-tab">
+    $('#bookEditPage').html(`<div class="card card-header">
+              <div class="row">
+              <div class="col-1">
+              <h4 >Book sold</h4></div>
+              <div class="col"> <input id="disabled" class="big-checkbox"  type="checkbox" ${disabled}></div></div></div>
+              <div class="tab-content" id="myTabContent">
+             <div class="tab-pane fade show active" id="name" role="tabpanel" aria-labelledby="name-tab">
              ${addPartsOfBook("name")}</div>
-             
             <div class="tab-pane fade" id="author" role="tabpanel" aria-labelledby="author-tab" >
              ${addPartsOfBook("author")} </div>
             <div class="tab-pane fade" id="description" role="tabpanel" aria-labelledby="description-tab">
@@ -128,17 +135,23 @@ function buildPage() {
             <div class="shadow p-4 mb-4 bg-white">
             <div id="divLoadAvatar">
             <h4>Avatar</h4>
-            <Label>Load avatar</Label>
-            <input type="file" class="form-control-file" id="avatar" accept=".jpg" onchange="loadImage('avatar','divAvatar')">
+            <div class="form-control-file" style="width: 18rem"">
+              <div id="carouselExampleCaptions"  class="carousel card-body slide w-50" data-ride="carousel">
+              <ol class="carousel-indicators" style="width: 18rem" id='test0'> </ol>
+              <div class="carousel-inner" style="width: 18rem" id='test1'></div></div>
+              <Label>Load cover</Label><br>
+              <input type="button" value="Choose file" style="width: 6rem" accept=".jpg"   onclick="setImgInCarousel()" ></input>
+              </div>
             </div>
-            <div class='car' id='divAvatar' style='width: 18rem;'>
-            </div><br><br></div>
+            </div><br><br>
             <div class="shadow p-4 mb-4 bg-white">
             <h4>Another Image</h4>
             <Label>Load another image</Label>
             <input type="file" class="form-control-file" id="loadAnotherImage" accept=".jpg" onchange="loadImage('loadAnotherImage','imageList')">
             <div class='car' id='imageList' style='width: 18rem;'>
-            </div></div></div></div></div>`);
+            </div></div></div></div></div></div>`);
+    divAvatar = $("#divAvatar");
+    listImages = $("#imageList");
     for (let tmpNameObject of nameObjectOfLocaleString) {
 
         html1 += `<div class="col card card-body my-2"><h5 class='bg-secondary p-2 text-white text-center'>${tmpNameObject}</h5>
@@ -172,40 +185,50 @@ function buildPage() {
         )
     }
 
-  /*  html1 += `<div class="card card-footer">
-              <div class="row  ">
-              <div class="col p-4 mb-4">
-              <h5> Year Of Edition </h5>
-              <input type="text" class="form-control" id='yearOfEdition' value=${tmpArr.yearOfEdition}></div>
-              <div class=" col p-4 mb-4 ">
-              <h5> Pages </h5>
-              <input type="number" class="form-control" id="pages" value=${tmpArr.pages} ></div>
-              <div class=" col p-4 mb-4  ">
-              <h5> Price </h5>
-              <input type="number" class="form-control" id="price" value=${tmpArr.price} ></div>
-              <div class="col p-4 mb-4 ">
-              <div class="row-cols-1">
-              <div class="col">
-              <h5> Original Language </h5></div>
-              <div class="col-4">
-              <select id="originalLanguage" class="form-control" value=${originalLang} ></select></div></div>
-              </div></div></div>`;*/
 
-   /* $('#bookEditPage').html(html1);*/
+    $('#inptname').attr("value", tmpArr.originalLanguage.name);
+    $('#inpnameru').attr("value", tmpArr.name.ru);
+    $('#inpnameen').attr("value", tmpArr.name.en);
+    $('#inpnamefr').attr("value", tmpArr.name.fr);
+    $('#inpnameit').attr("value", tmpArr.name.it);
+    $('#inpnamede').attr("value", tmpArr.name.de);
+    $('#inpnamecs').attr("value", tmpArr.name.cs);
+    $('#inpnamegr').attr("value", tmpArr.name.gr);
 
+    $('#inptauthor').attr("value", tmpArr.originalLanguage.author);
+    $('#inpauthorru').attr("value", tmpArr.author.ru);
+    $('#inpauthoren').attr("value", tmpArr.author.en);
+    $('#inpauthorfr').attr("value", tmpArr.author.fr);
+    $('#inpauthorit').attr("value", tmpArr.author.it);
+    $('#inpauthorde').attr("value", tmpArr.author.de);
+    $('#inpauthorcs').attr("value", tmpArr.author.cs);
+    $('#inpauthorgr').attr("value", tmpArr.author.gr);
+
+    $('#inpdescriptionru').attr("value", tmpArr.description.ru);
+    $('#inpdescriptionen').attr("value", tmpArr.description.en);
+    $('#inpdescriptionfr').attr("value", tmpArr.description.fr);
+    $('#inpdescriptionit').attr("value", tmpArr.description.it);
+    $('#inpdescriptionde').attr("value", tmpArr.description.de);
+    $('#inpdescriptioncs').attr("value", tmpArr.description.cs);
+    $('#inpdescriptiongr').attr("value", tmpArr.description.gr);
+
+    $('#inptedition').attr("value", tmpArr.originalLanguage.edition);
+    $('#inpeditionru').attr("value", tmpArr.edition.ru);
+    $('#inpeditionen').attr("value", tmpArr.edition.en);
+    $('#inpeditionfr').attr("value", tmpArr.edition.fr);
+    $('#inpeditionit').attr("value", tmpArr.edition.it);
+    $('#inpeditionde').attr("value", tmpArr.edition.de);
+    $('#inpeditioncs').attr("value", tmpArr.edition.cs);
+    $('#inpeditiongr').attr("value", tmpArr.edition.gr);
+
+    $('#yearOfEdition').attr("value", tmpArr.yearOfEdition);
+    $('#pages').attr("value", tmpArr.pages);
+    $('#price').attr("value", tmpArr.price);
+    document.getElementById('originalLanguage').value = tmpArr.originalLanguageName;
 
     $('#bookEditPageForImg').html(`
               <div class="row">
               <div class="col-4 ">
-              <div class="card" style="width: 21rem">
-              <div id="carouselExampleCaptions"  class="carousel card-body slide w-50" data-ride="carousel">
-              <ol class="carousel-indicators" style="width: 18rem" id='test0'> </ol>
-              <div class="carousel-inner" style="width: 18rem" id='test1'></div></div>
-              <div class="buttonCarousel card-footer ">
-              <buttonCarouselEdit class="left" href="#carouselExampleCaptions"  data-slide="prev" > <</buttonCarouselEdit>     
-              <buttonCarouselEdit class="right " href="#carouselExampleCaptions" data-slide="next" > ></buttonCarouselEdit>
-              <buttonCarouselEdit class="center "   onclick="setImgInCarousel()" > Upload file</buttonCarouselEdit></div>
-              </div>
               </div>
               <div class="col"> 
               <div class="card " style="width: 20rem;">
@@ -248,25 +271,37 @@ function showImage(x) {
 }
 
 function sendUpdateBook() {
-    var add = {};
-    add['id'] = idd;
-    for (let tmp of nameObjectOfLocaleString) {
-        var asd = {};
-        for (let tmpValue of nameVarOfLocaleStringWithId) {
-            asd[tmpValue] = $('#' + tmp + tmpValue).val();
+    let book = {};
+    book['id'] = idd;
+    let otherLangFields = {};
+    for (let tmpNameObject of nameObjectOfLocaleString) {
+        let bookFields = {};
+        for (let tmpNameVar of nameVarOfLocaleString) {
+            bookFields[tmpNameVar] = $("#inp" + tmpNameObject + tmpNameVar).val()
         }
-        add[tmp] = asd;
+        book[tmpNameObject] = bookFields;
+        if (tmpNameObject !== "description") {
+            otherLangFields[tmpNameObject] = $("#inpt" + tmpNameObject).val();
+            otherLangFields[tmpNameObject + "Translit"] = $("#in" + tmpNameObject).val();
+        }
     }
-    add['coverImage'] = nameImageCover;
-    add['imageList'] = listImages;
-    add['show'] = (!$("#disabled").is(':checked'));
-    add['yearOfEdition'] = $('#yearOfEdition').val();
-    add['pages'] = $('#pages').val();
-    add['price'] = $('#price').val();
-    add['originalLanguageName'] = $('#originalLanguage').val();
-
-    var body02 = JSON.stringify(add);
+    book["originalLanguage"] = otherLangFields;
+    book['coverImage'] = nameImageCover;
+    book['show'] = (!$("#disabled").is(':checked'));
+    book['yearOfEdition'] = $('#yearOfEdition').val();
+    book['pages'] = $('#pages').val();
+    book['price'] = $('#price').val();
+    book['originalLanguageName'] = $('#originalLanguage').val();
+    let allImages = $("#allImage").find("img");
+    let imageList = [];
+    for (let img of allImages) {
+        imageList.push(img.id);
+    }
+    book["listImage"] = imageList;
+    var body02 = JSON.stringify(book);
     sendUpdateBookReq(body02);
+    confirm("Edit this book?");
+    window.location.href = "http://localhost:8080/admin/panel/books";
 }
 
 async function sendUpdateBookReq(x) {
@@ -274,50 +309,18 @@ async function sendUpdateBookReq(x) {
         method: 'POST',
         body: x,
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json;charset=utf-8',
             'Accept': 'application/json'
         }
     });
 }
 
-function getVarBookDTO() {
-    fetch("/getVarBookDTO")
-        .then(status)
-        .then(json)
-        .then(function (resp) {
-            nameObjectOfLocaleString = Object.values(resp).filter(t => t !== "id");
-            getAllLocales();
-        });
-}
-
-function getAllLocales() {
-    fetch("lang")
-        .then(status)
-        .then(json)
-        .then(function (resp) {
-            nameVarOfLocaleStringWithId = resp;
-            nameVarOfLocaleStringWithId.unshift("id");
-            nameVarOfLocaleString = nameVarOfLocaleStringWithId.filter(t => t !== "id");
-            getBookDTOById(idd = localStorage.getItem('tmpEditBookId'));
-        });
-}
-
-function getBookDTOById(id) {
-    fetch("/api/book/" + id)
-        .then(status)
-        .then(json)
-        .then(function (resp) {
-            tmpArr = resp;
-            console.log(tmpArr);
-            buildPage();
-        });
-}
 
 function buildCarousel() {
     var countForActive = 0;
     var tmpHtmlForCarousel = '';
     var tmpHtmlForCarouselIndicators = '';
-    for (var i = 0; i < listImages.length; i++) {
+    for (var i = 1; i < listImages.length; i++) {
         if (listImages[i].nameImage !== '') {
             countForActive++;
             if (countForActive === 1) {
@@ -374,7 +377,6 @@ $("#exampleFormControlFile1").change(function () {
     uploadImageNew();
 });
 
-
 function uploadImageNew() {
     const formData = new FormData();
     const fileField = document.getElementById("exampleFormControlFile1");
@@ -391,4 +393,90 @@ function uploadImageNew() {
             buildCarousel();
         });
 }
+
+
+function loadImage(nameId, div) {
+    const formData = new FormData();
+    let fileImg = $("#" + nameId).prop('files')[0];
+    formData.append('file', fileImg);
+    fetch('/admin/upload', {
+        method: 'POST',
+        body: formData
+    })
+        .then(function (body) {
+            return body.text();
+        }).then(function (data) {
+        addImageInDiv(data, div);
+    });
+}
+
+function addImageInDiv(fileName, divId) {
+    let path = "/images/tmp/" + fileName;
+    let imageId = fileName.replace(/\./g, '');
+    if (divId === 'divAvatar') {
+        divAvatar.empty();
+        addImgAvatarAndBtn(fileName, path);
+    } else {
+        addImgToListAndBtn(fileName, path);
+    }
+}
+
+function addImgAvatarAndBtn(divId, path) {
+    divAvatar.append(
+        `<img src=${path} class='card-img-top' id=${divId} alt='...'>
+        <button type="button" onclick="deleteImage('divAvatar')" class="btn btn-primary mx-3">Delete image</button>`
+    )
+}
+
+function addImgToListAndBtn(divId, path) {
+    listImages.append(
+        `<div class="shadow p-4 mb-4 bg-white" id="${divId}">               
+                <img src=${path} id=${divId} class='card-img-top'  alt='...'>
+                <button type="button" onclick="deleteImage('${divId}')" class="btn btn-primary mx-3">Delete image</button><br>              
+                </div>`
+    )
+}
+
+function deleteImage(id) {
+    if (id === 'divAvatar') {
+        divAvatar.empty();
+    } else {
+        document.getElementById(id).remove();
+    }
+
+}
+
+function getVarBookDTO() {
+    fetch("/getVarBookDTO")
+        .then(status)
+        .then(json)
+        .then(function (resp) {
+            nameObjectOfLocaleString = Object.values(resp).filter(t => t !== "id");
+            getAllLocales();
+        });
+}
+
+function getAllLocales() {
+    fetch("lang")
+        .then(status)
+        .then(json)
+        .then(function (resp) {
+            nameVarOfLocaleStringWithId = resp;
+            nameVarOfLocaleStringWithId.unshift("id");
+            nameVarOfLocaleString = nameVarOfLocaleStringWithId.filter(t => t !== "id");
+            getBookDTOById(idd = localStorage.getItem('tmpEditBookId'));
+        });
+}
+
+function getBookDTOById(id) {
+    fetch("/api/book/" + id)
+        .then(status)
+        .then(json)
+        .then(function (resp) {
+            tmpArr = resp;
+            console.log(tmpArr);
+            buildPage();
+        });
+}
+
 
