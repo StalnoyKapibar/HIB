55ce296fe260d8b8ad78535b67928820d3965472
Лукьянов Роман Андреевич <vienoulis@gmail.com>
2020-05-15 19:39:20 +0300

    Первый комит

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+        //test
     }
 }
