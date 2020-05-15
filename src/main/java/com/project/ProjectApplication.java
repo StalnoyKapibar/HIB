729d81b302a1bbf07a5fdd2683c4d9676a4c29ed
Лукьянов Роман Андреевич <vienoulis@gmail.com>
2020-05-15 19:57:16 +0300

    Удалил тестовые изменения

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
-        //test
     }
 }
