cd8587755149cabc2fc4508201de5ff22e68361b
wakeupne0 <a.davidoff@outlook.com>
2020-05-20 01:14:59 +0300

    Добавил автоматическое выделение и снятие выделения подкатегорий

--- src/main/resources/static/js/utils/search.js
+++ src/main/resources/static/js/utils/search.js
@@ -4,6 +4,14 @@ let id = "";
 $(document).ready(function () {
     setPageFields();
     getCategoryTree();
+    $('#input-categories').on('click', '.custom-control-input', function() {
+
+        if ($(this).is(':checked')) {
+            $(this).parent().parent().find('.custom-control-input').prop('checked', true);
+        } else {
+            $(this).parent().parent().find('.custom-control-input').prop('checked', false);
+        }
+    });
 });
 
 function getCategoryTree() {
@@ -51,7 +59,7 @@ function setTreeView(category) {
         row =
             `<div class="category">
                 <div class="custom-control custom-checkbox form-check-inline" id="heading-${i}">
-                    <input class="custom-control-input collapsed" type="checkbox" id="check-${i}" value="${category[i].categoryName}">
+                    <input class="custom-control-input" type="checkbox" id="check-${i}" value="${category[i].categoryName}">
                     <label class="custom-control-label" for="check-${i}"></label>
                     <label data-toggle="collapse" data-target="#collapse-${i}" aria-expanded="false" aria-controls="collapse-${i}">
                        ${category[i].categoryName}
@@ -77,7 +85,7 @@ function setChilds(category, count) {
             row +=
                 `<div class="category">
                     <div class="custom-control custom-checkbox form-check-inline" id="heading-${id}${i}">
-                        <input class="custom-control-input collapsed" type="checkbox" id="check-${id}${i}" value="${category[i].categoryName}">
+                        <input class="custom-control-input" type="checkbox" id="check-${id}${i}" value="${category[i].categoryName}">
                         <label class="custom-control-label" for="check-${id}${i}">
                             ${category[i].categoryName}
                         </label>
@@ -87,7 +95,7 @@ function setChilds(category, count) {
             row +=
                 `<div class="category">
                     <div class="custom-control custom-checkbox form-check-inline" id="heading-${id}${i}">
-                        <input class="custom-control-input collapsed" type="checkbox" id="check-${id}${i}" value="${category[i].categoryName}">
+                        <input class="custom-control-input" type="checkbox" id="check-${id}${i}" value="${category[i].categoryName}">
                         <label class="custom-control-label" for="check-${id}${i}"></label>
                         <label data-toggle="collapse" data-target="#collapse-${id}${i}" aria-expanded="false" aria-controls="collapse-${id}${i}">
                            ${category[i].categoryName}
@@ -133,28 +141,6 @@ function advancedSearch() {
             addFindeBooks(data)
         });
 }
-
-// function advancedSearch() {
-//     let map = new Map();
-//     let categories = $("#input-categories input:checked").map(function(){
-//         return $(this).val();
-//     }).get();
-//     map.set("1", "str1");    // строка в качестве ключа
-//     map.set(1, "num1");      // цифра как ключ
-//     map.set(true, "bool1");  // булево значение как ключ
-//     map.entries();
-//     fetch("/searchAdvanced?categories=mgr&categories=20160210", {
-//         method: "GET",
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Accept': 'application/json'
-//         }
-//     })
-//         .then(data => data.json())
-//         .then(function (data) {
-//             addFindeBooks(data)
-//         });
-// }
 
 function setPageFields() {
     if (window.location.search === "") {
