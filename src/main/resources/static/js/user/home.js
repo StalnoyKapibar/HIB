5d694a18d84ac8cc575be64ec3f81f20ff4427ee
Лукьянов Роман Андреевич <vienoulis@gmail.com>
2020-05-20 03:42:37 +0300

    Но на 400 мне больше нравиться. + добавил отступы от ценника от кнопок.

--- src/main/resources/static/js/user/home.js
+++ src/main/resources/static/js/user/home.js
@@ -45,7 +45,7 @@ function getQuantityPage() {
           let cssOfBtn = listOrdersOfCart.includes(books[index].id) ? "disabled" : "addToCartBtn";
           let card = `<div class="col mb-4">
                                     <a class="card border-0" href="/page/${books[index].id}" style="color: black">
-                                        <img class="card-img-top mb-1" src="images/book${books[index].id}/${books[index].coverImage}" style="object-fit: contain; height: 350px; " alt="Card image cap">
+                                        <img class="card-img-top mb-1" src="images/book${books[index].id}/${books[index].coverImage}" style="object-fit: contain; height: 400px; " alt="Card image cap">
                                         <div class="card-body" style="padding-bottom: 30px">
                                             <h5 class="card-title">${convertOriginalLanguageRows(books[index].nameAuthorDTOLocale, books[index].authorTranslit)}</h5>
                                             <h6 class="card-text text-muted">${convertOriginalLanguageRows(books[index].nameBookDTOLocale, books[index].nameTranslit)}</h6>
