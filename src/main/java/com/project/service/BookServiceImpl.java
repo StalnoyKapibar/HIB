4b14b05f3fda0c2db9fe9225dd1dd73b6686da9f
wakeupne0 <a.davidoff@outlook.com>
2020-05-22 01:32:54 +0300

    fix поиска по автору

--- src/main/java/com/project/service/BookServiceImpl.java
+++ src/main/java/com/project/service/BookServiceImpl.java
@@ -75,6 +75,12 @@ public class BookServiceImpl implements BookService {
     @Override
     public BookNewDTO getBookBySearchRequest(String name, String translitName, OriginalLanguage originalLanguage, Long priceFrom, Long priceTo,
                                              String yearOfEditionFrom, String yearOfEditionTo, Long pagesFrom, Long pagesTo, String searchBy, List<String> categories) {
+        if (pagesFrom == null) {
+            pagesFrom = Long.valueOf(0);
+        }
+        if (pagesTo == null) {
+            pagesTo = Long.valueOf(0);
+        }
         return bookDAO.getBookBySearchRequest(name, translitName, originalLanguage, priceFrom, priceTo, yearOfEditionFrom, yearOfEditionTo, pagesFrom,
                 pagesTo, searchBy, categories);
     }
