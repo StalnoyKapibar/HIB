8cd165146bec2fad672feb90ea16e75103bd4e45
orlovleo12 <orlovleo12>
2020-05-26 12:10:53 -0500

    Починил возможность добавленияфотография в карусель при изменениях книги. Теперь добавление новых картинок в карусель, не удаляет старые.

--- src/main/resources/static/js/admin/buildEditPage.js
+++ src/main/resources/static/js/admin/buildEditPage.js
@@ -341,10 +341,23 @@ function sendUpdateBook() {
     for (let img of allImages) {
         imageList.push(img.id);
     }
+    let imageListTmpPattern = [];
+    for (let index = 0; index < imageList.length; index++) {
+        imageListTmpPattern[index] = JSON.parse(JSON.stringify(tmpArr.listImage[0]));
+    }
+    for (let index = 0; index < imageListTmpPattern.length; index++) {
+        imageListTmpPattern[index].id = 0;
+        imageListTmpPattern[index].nameImage = imageList[index];
+    }
+
     if (allImages.length == 0) {
         book["listImage"] = tmpArr.listImage;
     } else {
-        book["listImage"] = imageList;
+        let indexListImage = tmpArr.listImage.length;
+        for (let index = tmpArr.listImage.length + 1; index <= imageListTmpPattern.length + indexListImage; index++) {
+            tmpArr.listImage.push(imageListTmpPattern[index - indexListImage - 1]);
+        }
+        book["listImage"] = tmpArr.listImage;
     }
 
     var body02 = JSON.stringify(book);
@@ -426,6 +439,7 @@ function deleteTmpImage(x) {
         }
     });
 }
+
 function deleteCarouselImageFromDB(x) {
     var delTmp = tmpArr.listImage[x].id;
     delete tmpArr.listImage[x];
@@ -477,6 +491,7 @@ function uploadImageNew() {
 function loadImage(nameId, div) {
     const formData = new FormData();
     let fileImg = $("#" + nameId).prop('files')[0];
+
     formData.append('file', fileImg);
     fetch('/admin/upload', {
         method: 'POST',
