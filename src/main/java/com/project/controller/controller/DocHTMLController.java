f518bae61d2353c9b8b37444ec751f25a28e8c2d
Лукьянов Роман Андреевич <vienoulis@gmail.com>
2020-05-28 17:53:18 +0300

    Сделал Шаблон для Заказа

--- src/main/java/com/project/controller/controller/DocHTMLController.java
+++ src/main/java/com/project/controller/controller/DocHTMLController.java
@@ -1,24 +1,38 @@
 package com.project.controller.controller;
 
 import com.project.service.abstraction.BookService;
+import com.project.service.abstraction.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DocHTMLController {
 
-    private final BookService service;
+    private final BookService bookService;
+    private final OrderService orderService;
 
-    public DocHTMLController(BookService service) {
-        this.service = service;
+    public DocHTMLController(BookService bookService, OrderService orderService) {
+        this.bookService = bookService;
+        this.orderService = orderService;
     }
 
-    @GetMapping("/mail/document")
-    public String getHTMLDocument(long id, Model model){
-        model.addAttribute("book", service.getBookById(id) );
-        return "mailForm/test-mail";
+    @GetMapping("/mail/book")
+    public String getBook(@RequestParam long id, Model model){
+        model.addAttribute("book", bookService.getBookById(id) );
+        return "mailForm/book";
     }
 
+    @GetMapping("/mail/order")
+    public String getOrder(@RequestParam long id, Model model){
+        model.addAttribute("orders", orderService.getOrderById(id));
+        return "mailForm/order";
+    }
+
+    @GetMapping("mail/oneClick")
+    public String getOneClick(){
+        return "mailForm/oneClickReg";
+    }
 
 }
