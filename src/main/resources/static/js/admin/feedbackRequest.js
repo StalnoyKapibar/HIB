92e33d0580758857f0d07105bbe0340842161f88
orlovleo12 <orlovleo12>
2020-06-07 18:51:00 +0300

    Work chat with order

--- src/main/resources/static/js/admin/feedbackRequest.js
+++ src/main/resources/static/js/admin/feedbackRequest.js
@@ -12,6 +12,7 @@ let interestedBookImage = $("#interested-image");
 let interestedBookTitle = $("#interested-title");
 let toggleReplied = $("#toggle-replied");
 const localStorageToggleKey = "request-toggle";
+let allOrders;
 
 $(document).ready(function () {
     if (localStorage.getItem(localStorageToggleKey) === "true") {
@@ -20,6 +21,7 @@ $(document).ready(function () {
         getFeedbackRequestTable(false).then(r => {
         });
     }
+    showListOrders();
 });
 
 function markAsRead(id, replied) {
@@ -62,7 +64,10 @@ async function getFeedbackRequestTable(replied) {
                                data-message="${content}"
                                data-bookId="${bookId}"
                                data-bookName="${bookName}"
-                               data-bookCoverImage="${bookCoverImage}">Reply</button>`;
+                               data-bookCoverImage="${bookCoverImage}"
+                               onclick="showModalOfOrder(${id})">Reply</button>
+         `
+                                ;
                     mark = `<button type="button"
                             class="btn btn-info "           
                             onclick="markAsRead(${id},${data[i].replied})">Read</button>`;
@@ -158,4 +163,122 @@ toggleReplied.change(() => {
     localStorage.setItem(localStorageToggleKey, repliedOn.toString());
     getFeedbackRequestTable(repliedOn).catch();
 });
+
+async function showModalOfOrder(index) {
+    $('#chat').empty();
+    $('#modalBody').empty();
+    $('#contactsOfUser').empty();
+    orderIndex = index;
+    let order = allOrders[index-1];
+    let items = order.items;
+    $('#modalTitle').html(`Order № ${order.id}`);
+    messagePackIndex = 0;
+    document.getElementById("chat").setAttribute('onscroll', 'scrolling()');
+
+    let htmlChat = ``;
+    await fetch("/gmail/" + order.contacts.email + "/messages/" + "0")
+        .then(json)
+        .then((data) => {
+            if (data[0] === undefined) {
+                htmlChat += `<div id="chat-wrapper">`;
+                htmlChat += `</div>`;
+                htmlChat += `<textarea id="sent-message" class="form-control"></textarea>
+                        </div><button class="float-right col-2 button btn-primary" type="button" id="send-button" onclick="sendGmailMessage('${order.contacts.email}', ${index})">Send</button>`
+            } else {
+                if (data[0].text === "noGmailAccess") {
+                    htmlChat += `<div>
+                                <span class="h3 col-10">Confirm gmail access to open chat:</span>
+                                <a type="button" class="col-2 btn btn-primary float-right" href="${gmailAccessUrl.fullUrl}">
+                                confirm</a>
+                            </div>`
+                } else {
+                    htmlChat += `<div id="chat-wrapper">`;
+                    for (let i = data.length - 1; i > -1; i--) {
+                        htmlChat += `<p><b>${data[i].sender}</b></p>
+                    <p>${data[i].text}</p>`
+                    }
+                    htmlChat += `</div>`;
+                    htmlChat += `<textarea id="sent-message" class="form-control"></textarea>
+                        </div><button class="float-right col-2 button btn-primary" type="button" id="send-button" onclick="sendGmailMessage('${order.contacts.email}', ${index})">Send</button>`
+                }
+            }
+        });
+    $('#chat').html(htmlChat);
+    $('#chat').scrollTop(1000);
+
+    let html = ``;
+    html += `<thead><tr><th>Image</th>
+                             <th>Name | Author</th>
+                             <th></th>
+                             <th>Price</th></tr></thead>`;
+    $.each(items, function (index) {
+        let book = items[index].book;
+        html += `<tr><td class="align-middle"><img src="/images/book${book.id}/${book.coverImage}" style="max-width: 80px"></td>
+                             <td width="350">${convertOriginalLanguageRows(book.originalLanguage.name, book.originalLanguage.nameTranslit)} | ${convertOriginalLanguageRows(book.originalLanguage.author, book.originalLanguage.authorTranslit)}</td>
+                             <td></td>
+                             <td>${convertPrice(book.price)}${iconOfPrice}</td></tr>`;
+    });
+    html += `<tr><td></td><td></td><td>Subtotal :</td><td> ${convertPrice(order.itemsCost)}${iconOfPrice}</td></tr>
+                 <tr><td></td><td></td><td>Total :</td><td>${convertPrice(order.itemsCost + order.shippingCost)}${iconOfPrice}</td></tr>`;
+    $('#modalBody').html(html);
+
+    let htmlContact = ``;
+    htmlContact += `<div class="panel panel-primary">
+                        <div class="panel-body">
+                            <div class="container mt-2">
+                                <div class="col-8 p-4 mb-4  alert alert-info" role="alert">
+                                    <h6>User <strong>contacts </strong></h6>
+                                </div>`;
+    for (let key in order.contacts) {
+        if (order.contacts[key] !== "" && key !== "id" && key !== "comment") {
+            htmlContact += `<div class="form-group row">
+                        <label class="control-label col-sm-2 col-form-label">${key}</label>
+                        <div class="col-md-5 pl-0 pr-1">
+                            <input class="form-control" readonly  placeholder=${order.contacts[key]}>
+                        </div>
+                    </div>`;
+        }
+    }
+    if (order.comment !== " ") {
+        htmlContact += `<div class="form-group row">
+                        <label class="control-label col-sm-2 col-form-label">Comment</label>
+                        <div class="col-md-6 pl-0">
+                            <textarea class="form-control" readonly  rows="5" placeholder="${order.comment}" ></textarea>
+                        </div>
+                    </div>`;
+    }
+
+    htmlContact += `</div></div>`;
+
+    $('#contactsOfUser').html(htmlContact);
+}
+
+async function scrolling() {
+    let order = allOrders[orderIndex];
+    if ($('#chat').scrollTop() < 2) {
+        messagePackIndex++;
+        await fetch("/gmail/" + order.contacts.email + "/messages/" + messagePackIndex)
+            .then(json)
+            .then((data) => {
+                if (data[0].text === "chat end") {
+                    document.getElementById("chat").removeAttribute('onscroll');
+                    return;
+                }
+                for (let i = 0; i < data.length; i++) {
+                    let html = `<p><b>${data[i].sender}</b></p>
+                    <p>${data[i].text}</p>`
+                    document.getElementById("chat-wrapper").insertAdjacentHTML("afterbegin", html);
+                }
+            });
+    }
+}
+
+function showListOrders() {
+    fetch("/api/admin/getAllOrders")
+        .then(json)
+        .then(function (data) {
+
+            allOrders = data;
+        });
+}
 
