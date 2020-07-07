665e276fed433829d4fdbc9d5678d762b572b19f
Pavel-Alexandrov <alexpavtail@gmail.com>
2020-07-07 20:26:24 +0300

    Фикс парсера Gmail почты

--- src/main/java/com/project/controller/restcontroller/emailUtil/emailParser/GmailParser.java
+++ src/main/java/com/project/controller/restcontroller/emailUtil/emailParser/GmailParser.java
@@ -7,10 +7,10 @@ import java.util.regex.Pattern;
 public class GmailParser implements EmailParser {
     @Override
     public String getMessageTextWithoutRe(String text) {
-        Pattern pattern = Pattern.compile("<div class=\"gmail_quote.+</blockquote>");
+        Pattern pattern = Pattern.compile("<div class=\"gmail_quote");
         Matcher matcher = pattern.matcher(text);
         if (matcher.find()) {
-            String re = text.substring(text.indexOf("<div class=\"gmail_quote"), text.indexOf("</blockquote></div>")+19);
+            String re = text.substring(text.indexOf("<div class=\"gmail_quote"));
             text = text.replace(re, "");
         }
         return text;
