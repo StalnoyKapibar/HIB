3f717d9b20e704595c158833200a9b7778c248e5
Roman <rvs_dk@mail.ru>
2020-07-21 12:54:35 +0300

    Возвращается ответ не из БД

--- src/main/java/com/project/dao/FormErrorMessageDaoImpl.java
+++ src/main/java/com/project/dao/FormErrorMessageDaoImpl.java
@@ -14,16 +14,11 @@ public class FormErrorMessageDaoImpl extends AbstractDao<Long, FormLoginErrorMes
 
     @Override
     public FormLoginErrorMessageDTO getErrorMessage(String field, String reason, String locale) {
-        // TODO: 09.07.2020  Блок if убрать, добавить запись в БД: auth, UserDisabled, message 
-        if (reason.equals("UserDisabled")) {
-            return new FormLoginErrorMessageDTO(true, "Пожалуйста, подтвердите Ваш e-mail. Для этого перейдите по ссылке из письма.");
-        }
         String HQL = "SELECT new com.project.model.FormLoginErrorMessageDTO(true, fem.body.LOCALE) ".replaceAll("LOCALE", locale) +
                      "FROM FormLoginErrorMessage fem " +
                      "WHERE fem.field=:field AND fem.reason=:reason";
-        FormLoginErrorMessageDTO singleResult = entityManager.createQuery(HQL, FormLoginErrorMessageDTO.class)
+        return entityManager.createQuery(HQL, FormLoginErrorMessageDTO.class)
                 .setParameter("field", field)
                 .setParameter("reason", reason).getSingleResult();
-        return singleResult;
     }
 }
