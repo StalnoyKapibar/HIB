3dccb5b9c716aadcbc06efb0f5ad2d3afef3aad1
Max <mixamus@me.com>
2020-07-28 10:59:57 +0300

    teest 43

--- src/main/java/com/project/controller/controller/ViewController.java
+++ src/main/java/com/project/controller/controller/ViewController.java
@@ -48,7 +48,7 @@ public class ViewController {
             modelAndView.setViewName("redirect:/err/not-found");
             return modelAndView;
         }
-            modelAndView.addObject("isPage", true);
+            modelAndView.addObject("isPage", 1);
             modelAndView.addObject("book", id);
             modelAndView.addObject("pageBook", book);
             modelAndView.setViewName("user/user-page");
