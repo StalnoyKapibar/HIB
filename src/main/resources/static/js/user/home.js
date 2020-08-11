e80e8f28f32a296d3f28a6f1c4e88bf2809fb9b7
hadarych9 <ale-khadarov@yandex.ru>
2020-08-11 14:26:22 +0300

    Add-Edit-Page and Pictures fix -Поправлены пробелы

--- src/main/resources/static/js/user/home.js
+++ src/main/resources/static/js/user/home.js
@@ -67,33 +67,30 @@ async function addBooksToPage(books) {
             coverImageLink = 'images/book' + books[index].id + '/' + books[index].coverImage;
         }
         let card = `<div class="col mb-4">
-                                    <a class="card border-0" href="/page/${books[index].id}" style="color: black">
-                                        
-                                        <img class="card-img-top mb-1" src=${coverImageLink} style="object-fit: contain; height: 400px; ${books[index].show === true ? '' : 'opacity: 0.3;'}" alt="Card image cap">
-                                        
-                                        <div class="card-body" style="padding-bottom: 30px">
-                                            <h6 class="card-title">${convertOriginalLanguageRows(books[index].nameBookDTOLocale, books[index].nameTranslit)}</h6>
-                                            <h7 class="card-text text-muted">${convertOriginalLanguageRows(books[index].nameAuthorDTOLocale, books[index].authorTranslit)}</h7>
-                                            <h6 class="card-footer bg-transparent text-left pl-0">${covertPrice(books[index].price) + currencyIcon}</h6>
-                                            
-                                        </div>
-                                    </a>
-                                    ${isAdmin ? `<div style="position: absolute; bottom: 5px; left: 15px; right: 15px" id="bottomEditBook" type="button" 
-                                                    class="btn btn-info"
-                                                    onclick="openEdit(${books[index].id})">                        
-                                                    ${editBook}
-                                                  </div>`
+        <a class="card border-0" href="/page/${books[index].id}" style="color: black">
+            <img class="card-img-top mb-1" src=${coverImageLink} style="object-fit: contain; height: 400px; ${books[index].show === true ? '' : 'opacity: 0.3;'}" alt="Card image cap">
+            <div class="card-body" style="padding-bottom: 30px">
+                <h6 class="card-title">${convertOriginalLanguageRows(books[index].nameBookDTOLocale, books[index].nameTranslit)}</h6>
+                <h7 class="card-text text-muted">${convertOriginalLanguageRows(books[index].nameAuthorDTOLocale, books[index].authorTranslit)}</h7>
+                <h6 class="card-footer bg-transparent text-left pl-0">${covertPrice(books[index].price) + currencyIcon}</h6>
+            </div>
+        </a>
+        ${isAdmin ? `<div style="position: absolute; bottom: 5px; left: 15px; right: 15px" id="bottomEditBook" type="button" 
+        class="btn btn-info"
+        onclick="openEdit(${books[index].id})">                        
+        ${editBook}
+        </div>`
             : books[index].show === true
                 ? `<button style="position: absolute; bottom: 5px; left: 15px; right: 15px" id="bottomInCart width: -moz-available"
-                                                      class="btn btn-success ${cssOfBtn} btn-metro"  data-id="${books[index].id}">                        
-                                                    ${textOfBtn}
-                                                </button>`
+                       class="btn btn-success ${cssOfBtn} btn-metro"  data-id="${books[index].id}">                        
+                       ${textOfBtn}
+                   </button>`
                 : `<button style="position: absolute; bottom: 5px; left: 15px; right: 15px" id="bottomInCart" 
-                                                      class="btn btn-light btn-metro bought-btn-loc"  data-id="${books[index].id}">                        
-                                                    Out of stock
-                                                </button>`
+                       class="btn btn-light btn-metro bought-btn-loc"  data-id="${books[index].id}">                        
+                       Out of stock
+                   </button>`
         }
-                                 </div>`;
+        </div>`;
         $('#cardcolumns').append(card);
     });
     addPagination();
