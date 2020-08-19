d05a2737bea1d516cb4686dfce071e1c6e10c9a4
Kirill Timofeev <kirill19966881@gmail.com>
2020-08-19 18:47:26 +0300

    1 Теперь редирект идет в зависимости от среды.

--- src/main/java/com/project/controller/controller/GmailController.java
+++ src/main/java/com/project/controller/controller/GmailController.java
@@ -3,6 +3,7 @@ package com.project.controller.controller;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.gmail.Gmail;
@@ -14,12 +15,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Controller
 @PropertySource("classpath:gmail.properties")
 public class GmailController {
 
+    @Value("${clientIdloc}")
+    private String clientIdloc;
+    @Value("${clientSecretloc}")
+    private String clientSecretloc;
+    @Value("${redirectUriloc}")
+    private String redirectUriloc;
     @Value("${clientId}")
     private String clientId;
     @Value("${clientSecret}")
@@ -28,20 +36,36 @@ public class GmailController {
     private String redirectUri;
 
     @GetMapping(value = "/gmail/admin", params = "code")
-    public String handleGoogleAccess(@RequestParam("code") String response) throws IOException {
-        GmailRestController.code = response;
-        GoogleTokenResponse googleTokenResponse = new GoogleAuthorizationCodeTokenRequest(new NetHttpTransport(),
-                new JacksonFactory(), clientId, clientSecret, response, redirectUri).execute();
+    public String handleGoogleAccess(HttpServletRequest request, @RequestParam("code") String response) throws IOException {
+        String url = request.getRequestURL().toString();
 
-        GoogleCredential googleCredential = new GoogleCredential.Builder()
-                .setTransport(new NetHttpTransport())
-                .setJsonFactory(new JacksonFactory())
-                .setClientSecrets(clientId, clientSecret)
-                .build()
-                .setFromTokenResponse(googleTokenResponse);
-        Gmail buildGmail = new Gmail.Builder(new NetHttpTransport(), new JacksonFactory(), googleCredential).build();
-        GmailRestController.gmail = buildGmail;
-        ParseGmailController.gmail = buildGmail;
+        GmailRestController.code = response;
+        if (url.contains("localhost")) {
+            GoogleTokenResponse googleTokenResponse = new GoogleAuthorizationCodeTokenRequest(new NetHttpTransport(),
+                    new JacksonFactory(), clientIdloc, clientSecretloc, response, redirectUriloc).execute();
+                GoogleCredential googleCredential = new GoogleCredential.Builder()
+                        .setTransport(new NetHttpTransport())
+                        .setJsonFactory(new JacksonFactory())
+                        .setClientSecrets(clientIdloc, clientSecretloc)
+                        .build()
+                        .setFromTokenResponse(googleTokenResponse);
+            Gmail buildGmail = new Gmail.Builder(new NetHttpTransport(), new JacksonFactory(), googleCredential).build();
+            GmailRestController.gmail = buildGmail;
+            ParseGmailController.gmail = buildGmail;
+        } else {
+            GoogleTokenResponse googleTokenResponse = new GoogleAuthorizationCodeTokenRequest(new NetHttpTransport(),
+                    new JacksonFactory(), clientId, clientSecret, response, redirectUri).execute();
+
+            GoogleCredential googleCredential = new GoogleCredential.Builder()
+                    .setTransport(new NetHttpTransport())
+                    .setJsonFactory(new JacksonFactory())
+                    .setClientSecrets(clientId, clientSecret)
+                    .build()
+                    .setFromTokenResponse(googleTokenResponse);
+            Gmail buildGmail = new Gmail.Builder(new NetHttpTransport(), new JacksonFactory(), googleCredential).build();
+            GmailRestController.gmail = buildGmail;
+            ParseGmailController.gmail = buildGmail;
+        }
         return "admin/admin-page";
     }
 
--- src/main/resources/static/js/admin/admin-nav.js
+++ src/main/resources/static/js/admin/admin-nav.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-    document.getElementById("gmail-access").href = gmailAccessUrl.fullUrl;
+    let url = window.location.href;
+    if (url.indexOf('localhost') > -1) {
+        document.getElementById("gmail-access").href = gmailAccessUrlloc.fullUrl;
+    } else {
+        document.getElementById("gmail-access").href = gmailAccessUrlserv.fullUrl;
+    }
 });
+
+
+/*
+$(document).ready(function () {
+    if (document.location.indexOf('localhost') > -1) {
+        $(document).ready(function () {
+            document.getElementById("gmail-access").href = gmailAccessUrlloc.fullUrl;
+
+        });
+    } else {
+        $(document).ready(function () {
+            document.getElementById("gmail-access").href = gmailAccessUrlserv.fullUrl;
+        });
+    }
+});
+
+ */
