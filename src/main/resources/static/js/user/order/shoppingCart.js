47050d2ac75182b6e1b8a21c24a21afb84172ec0
hadarych9 <ale-khadarov@yandex.ru>
2020-09-07 07:35:13 +0300

    Визуальные патчи для кнопок и микро-фиксы

--- src/main/resources/static/js/user/order/shoppingCart.js
+++ src/main/resources/static/js/user/order/shoppingCart.js
@@ -71,21 +71,21 @@ async function getShoppingCart() {
                 }
                 if (!isOrderEnable) {
                     $('#shoppingCardOrderDisabledMessage').addClass('resolveShopCart').text('Please resolve shopping cart warnings before proceeding');
-                    $('#forButtonCheckout').html(`<div><button class="btn btn-primary checkout-btn" id="chechout" onclick="confirmAddress()" type="button" disabled="disabled">
+                    $('#forButtonCheckout').html(`<div><button class="btn btn-primary checkout-btn mt-3" onclick="confirmAddress()" type="button" disabled="disabled">
                                     Checkout
                                 </button></div>`);
-                    $('#for-1click-reg').html(`<button class="btn btn-primary" id="1click-reg-btn"
+                    $('#for-1click-reg').html(`<button class="btn btn-secondary mt-3" id="1click-reg-btn"
                                                onclick="location.href='/1clickreg'" type="button" disabled="disabled">
                                                Buy without sign up</button>`);
                 } else {
                     $('#shoppingCardOrderDisabledMessage').text('');
-                    $('#forButtonCheckout').html(`<div><button class="btn btn-primary checkout-btn" id="chechout" onclick="confirmAddress()" type="button">
+                    $('#forButtonCheckout').html(`<div><button class="btn btn-primary checkout-btn mt-3" onclick="confirmAddress()" type="button">
                                     Checkout
                                 </button></div>`);
                     // $('#for-1click-reg').html(`<button class="btn btn-primary" id="1click-reg-btn"
                     //                            onclick="location.href='/1clickreg'" type="button">
                     //                            Buy without sign up</button>`);
-                    $('#for-1click-reg').html(`<button class="btn btn-primary" id="1click-reg-btn"
+                    $('#for-1click-reg').html(`<button class="btn btn-secondary mt-3" id="1click-reg-btn"
                                                onclick="showContacts1ClickReg()" type="button">
                                                Buy without sign up</button>`);
                 }
