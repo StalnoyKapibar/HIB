8dcbc6e38829b94b3a9dbd12b41968759a79bd7d
hadarych9 <58065098+hadarych9@users.noreply.github.com>
2020-09-09 16:19:22 +0300

    Revert "fixsearch"

--- src/main/java/com/project/model/Book.java
+++ src/main/java/com/project/model/Book.java
@@ -43,7 +43,7 @@ public class Book {
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private LocaleString edition;
 
-    private Long yearOfEdition;
+    private String yearOfEdition;
 
     private Long pages;
 
