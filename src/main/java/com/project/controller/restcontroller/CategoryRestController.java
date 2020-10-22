163761fc40d8a6bdacdb1222945e9277395d4bdf
frutodelaranja <frutodelaranja@gmail.com>
2020-10-23 01:17:55 +0300

    Сваггер аннотации в контроллере CategoryRestController

--- src/main/java/com/project/controller/restcontroller/CategoryRestController.java
+++ src/main/java/com/project/controller/restcontroller/CategoryRestController.java
@@ -3,6 +3,8 @@ package com.project.controller.restcontroller;
 import com.project.model.*;
 import com.project.service.CategoryService;
 import com.project.service.abstraction.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Api(tags = "This is the REST-API documentation for book categories.")
 @RestController
 @RequestMapping("/categories")
 @AllArgsConstructor
@@ -19,16 +22,19 @@ public class CategoryRestController {
 
     BookService bookService;
 
+    @ApiOperation(value = "Get list categories for name:", response = Iterable.class, tags = "getPanelCategories")
     @GetMapping("/getpanelcategories/{name}")
     public List<Category> getPanelCategories(@PathVariable("name") String name) {
         return categoryService.getListCategoriesById(name);
     }
 
+    @ApiOperation(value = "Get category tree by locale", response = Iterable.class, tags = "getCategoryTreeByLocale")
     @GetMapping("/gettree/{loc}")
     public List getCategoryTree(@PathVariable("loc") String loc) {
         return categoryService.getCategoryTree(loc);
     }
 
+    @ApiOperation(value = "Get list books by path", response = Iterable.class, tags = "getBooksByPath")
     @GetMapping("/getbooks")
     public List<BookDTOForCategories> getBooksByPath(@RequestParam("path") String path) {
         List<BookDTOForCategories> books = new ArrayList<>();
@@ -38,6 +44,7 @@ public class CategoryRestController {
         return books;
     }
 
+    @ApiOperation(value = "Get count books", response = Long.class, tags = "getCountBooksByPath")
     @GetMapping("/getcount")
     public Long getCountBooksByPath(@RequestParam("path") String path, @RequestParam("show") boolean isShow) {
         long i = 0;
--- src/main/java/com/project/controller/restcontroller/PageBookRestController.java
+++ src/main/java/com/project/controller/restcontroller/PageBookRestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(value = "Swagger2GetBookForPageBook")
+@Api(tags = "This is the REST-API documentation for the book page")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/page/{id}")
