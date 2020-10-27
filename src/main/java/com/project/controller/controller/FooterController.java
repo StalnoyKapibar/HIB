a77ebcce1b9ccab3ac0236d0cca9e45e8026051d
frutodelaranja <frutodelaranja@gmail.com>
2020-10-27 15:57:18 +0300

    добавил FooterContactsRestController

--- src/main/java/com/project/controller/controller/FooterController.java
+++ src/main/java/com/project/controller/controller/FooterController.java
@@ -12,34 +12,34 @@ import java.util.Properties;
 
 @Controller
 public class FooterController {
-    @GetMapping("/guide")
-    public String getGuidePage() {
-        return "guide";
-    }
-
-    @GetMapping("/contacts")
-    public String getContactsPage(Model model) throws IOException {
-        Properties properties = new Properties();
-        properties.load(new FileInputStream("src/main/resources/footer.properties"));
-        String email = properties.getProperty("email");
-        String phoneNumber = properties.getProperty("phoneNumber");
-        model.addAttribute("email", email);
-        model.addAttribute("phoneNumber", phoneNumber);
-        return "contacts";
-    }
-
-    @GetMapping("/authors")
-    public String getAuthorsPage() {
-        return "authors";
-    }
-
-    @GetMapping("/manual")
-    public String getManualPage() {
-        return "manual";
-    }
-
-    @GetMapping("/links")
-    public String getLinksPage() {
-        return "links";
-    }
+//    @GetMapping("/guide")
+//    public String getGuidePage() {
+//        return "guide";
+//    }
+//
+//    @GetMapping("/contacts")
+//    public String getContactsPage(Model model) throws IOException {
+//        Properties properties = new Properties();
+//        properties.load(new FileInputStream("src/main/resources/footer.properties"));
+//        String email = properties.getProperty("email");
+//        String phoneNumber = properties.getProperty("phoneNumber");
+//        model.addAttribute("email", email);
+//        model.addAttribute("phoneNumber", phoneNumber);
+//        return "contacts";
+//    }
+//
+//    @GetMapping("/authors")
+//    public String getAuthorsPage() {
+//        return "authors";
+//    }
+//
+//    @GetMapping("/manual")
+//    public String getManualPage() {
+//        return "manual";
+//    }
+//
+//    @GetMapping("/links")
+//    public String getLinksPage() {
+//        return "links";
+//    }
 }
---
+++ src/main/java/com/project/model/FooterContactsDTO.java
@@ -0,0 +1,13 @@
+package com.project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FooterContactsDTO {
+    String email;
+    String phoneNumber;
+}
