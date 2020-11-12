348b782e6971d11d6785043f6191be5952003604
frutodelaranja <frutodelaranja@gmail.com>
2020-11-12 20:04:49 +0300

    тестирование  CORS в ResetPasswordRestController_0.1

--- src/main/java/com/project/controller/restcontroller/swagger_annotated/ResetPasswordRestController.java
+++ src/main/java/com/project/controller/restcontroller/swagger_annotated/ResetPasswordRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 
 @Api(tags = "REST-API документ, описывающий сброс пароля пользователя")
+@CrossOrigin(origins = "*")
 @RestController
 public class ResetPasswordRestController {
 
