2c7986468d60c1e90b5b50c980fc208bf43b9c4d
frutodelaranja <frutodelaranja@gmail.com>
2020-11-12 21:51:41 +0300

    тестирование  CORS в приложении_0.5

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -18,7 +18,7 @@ public class ProjectApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/registration").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
