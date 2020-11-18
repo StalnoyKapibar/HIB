752304930dd3dddcd833bcfb5a95146ab1a41399
Uetal <uetal@me.com>
2020-11-18 22:02:04 +0300

    links footer

--- src/main/java/com/project/controller/controller/ViewController.java
+++ src/main/java/com/project/controller/controller/ViewController.java
@@ -25,7 +25,8 @@ public class ViewController {
     FormLoginErrorMessageService messageService;
     BookService bookService;
 
-    @GetMapping({"/home", "/profile/**", "/category/**", "/shopping-cart", "/search", "/err/**", "/manual", "/guide"})
+    @GetMapping({"/home", "/profile/**", "/category/**", "/shopping-cart", "/search",
+            "/err/**", "/manual", "/guide", "/authors", "/links"})
     public String getHomePage() {
         return "user/user-page";
     }
