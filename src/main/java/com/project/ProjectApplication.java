3281ae253765df56c54259ec2fb4e772d6fd63c5
alexhab <alex-habba@mail.ru>
2020-11-25 21:26:44 +0300

    изменение Cors

--- src/main/java/com/project/ProjectApplication.java
+++ src/main/java/com/project/ProjectApplication.java
@@ -13,12 +13,14 @@ public class ProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**");
+//                        .allowedOrigins("*");
             }
         };
     }
