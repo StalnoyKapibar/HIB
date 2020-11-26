17b157899ca11472c4c1fb3a63e3c93a34ea21fb
alexhab <alex-habba@mail.ru>
2020-11-26 09:38:56 +0300

    changed settings CORS

--- src/main/java/com/project/filter/FilterCors.java
+++ src/main/java/com/project/filter/FilterCors.java
@@ -19,7 +19,8 @@ public class FilterCors implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         chain.doFilter(req, res);
     }
 
