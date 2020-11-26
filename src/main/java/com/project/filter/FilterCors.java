65226fd734166f3ec753e3d8d6fee9bbe3a5f3f6
alexhab <alex-habba@mail.ru>
2020-11-26 14:47:39 +0300

    changed settings CORS v2

--- src/main/java/com/project/filter/FilterCors.java
+++ src/main/java/com/project/filter/FilterCors.java
@@ -1,12 +1,16 @@
 package com.project.filter;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class FilterCors implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -16,12 +20,18 @@ public class FilterCors implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        chain.doFilter(req, res);
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, Authorization, X-Requested-With, Content-Type");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
     }
 
     @Override
