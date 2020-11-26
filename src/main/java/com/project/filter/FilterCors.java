1e06fc13ac23592042ac267a24dc679cac4f8552
alexhab <alex-habba@mail.ru>
2020-11-26 14:55:33 +0300

    changed settings CORS v3

--- src/main/java/com/project/filter/FilterCors.java
+++ src/main/java/com/project/filter/FilterCors.java
@@ -21,7 +21,8 @@ public class FilterCors implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
