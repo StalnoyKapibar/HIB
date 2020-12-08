dfd942aa8ba718327a92d671ea9dae136d20690d
alexhab <alex-habba@mail.ru>
2020-12-08 19:56:37 +0300

    add in ConsumerKafkaServiceImpl field TelegramBot

--- src/main/java/com/project/service/serviceKafka/ConsumerKafkaServiceImpl.java
+++ src/main/java/com/project/service/serviceKafka/ConsumerKafkaServiceImpl.java
@@ -1,14 +1,20 @@
 package com.project.service.serviceKafka;
 
 import com.project.service.abstraction.ConsumerKafkaService;
+import com.project.telegrambot.TelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsumerKafkaServiceImpl implements ConsumerKafkaService {
 
+    @Autowired
+    private TelegramBot telegramBot;
+
     @KafkaListener(topics = "myTopic", groupId = "consumerServer")
     public void listenGroup(String message) {
+        telegramBot.sendMessageFromKafka(Long.parseLong(message));
         System.out.println(message);
     }
 }
