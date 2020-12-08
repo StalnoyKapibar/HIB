608949414f9407b54eddf43bfea78e8d5bf6a2cc
Uetal <uetal@me.com>
2020-12-08 15:17:47 +0300

    3.5

--- src/main/java/com/project/telegrambot/TelegramBot.java
+++ src/main/java/com/project/telegrambot/TelegramBot.java
@@ -61,31 +61,143 @@ public class TelegramBot extends TelegramLongPollingBot {
      */
     @Override
     public void onUpdateReceived(Update update) {
-        boolean isAuthorized = false;
-
-
-
         SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId());
+        if (update.getMessage().getText().equals("/start")){
+            sendMessage.setText("приветственное сообщение, предлагает зарегистрироваться");
+        }
+        /*
+        1) Проверка наличия chatId в БД
+        2) Если есть, то показать статус всех заказов
+        3) Если нет, то авторизоваться по номеру телефона, сохранить chatId в БД, показать все заказы
+        4) Если по номеру телефона не найдено - предложить ввести логин/пароль с сайта
+         */
+
+
+        /*
+        1), 2)
+         */
+        boolean isAuthorized = false;
+        /*
+        3)
+         */
+        if (update.getMessage().getText().equals("registration")) {
+            sendMessage.setText("предлагает ввести номер телефона");
+        }
+        /**
+         * Поиск в БД заказа, по введенному номеру телефона
+         */
+        List<Order> orderList= orderService.getOrderByUserPhoneInContacts(update.getMessage().getText());
+        /*
+        Нужно добавить сохранение chatId в БД
+         */
+        String responseMessage = "";
+        if (isAuthorized) {
+            if (orderList != null) {
+                responseMessage += "Колличество заказов: " + orderList.size() + "\n";
+                for (Order order : orderList) {
+                    responseMessage += "id: " + order.getId() + "\n";
+                    responseMessage += "data: " + new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.US).format(order.getData()) + "\n";
+                    responseMessage += "address: " + order.getAddress() + "\n";
+                    responseMessage += "itemsCost: " + order.getItemsCost() + "\n";
+                    responseMessage += "trackingNumber: " + order.getTrackingNumber() + "\n";
+                    responseMessage += "status: " + order.getStatus() + "\n";
+                    responseMessage += "contacts: " + order.getContacts() + "\n";
+                    responseMessage += "comment: " + order.getComment() + "\n";
+                }
+                sendMessage.setText(responseMessage);
+            } else {
+                responseMessage+="не найдено заказов по указанному номеру: " + update.getMessage().getText()+"\n";
+                responseMessage+="предлагает ввеси логин пароль с сайта в одном сообщении через пробел";
+                sendMessage.setText(responseMessage);
+            }
+        }
+
+        /**
+         Отправка сообщения
+         */
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
 
 
 
+//        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+//        List < List < InlineKeyboardButton >> rowsInline = new ArrayList < > ();
+//        List < InlineKeyboardButton > rowInline = new ArrayList < > ();
+//        rowInline.add(new InlineKeyboardButton().setText("Open Browser").setCallbackData("1"));
+//        rowsInline.add(rowInline);
+//        markupInline.setKeyboard(rowsInline);
+//        sendMessage.setReplyMarkup(markupInline);
 
-//        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
-//
-//        List<InlineKeyboardButton> buttons1 = new ArrayList<>();
-//        buttons1.add(new InlineKeyboardButton().setText("Авторизация").setCallbackData("/autorize"));
-//        buttons.add(buttons1);
-//
-//        InlineKeyboardMarkup markupKeyboard = new InlineKeyboardMarkup();
-//        markupKeyboard.setKeyboard(buttons);
+//        update.getUpdateId();
+//        String id = update.getMessage().getChatId().toString();
 
+
+
+//        if (update.getMessage().getText().equals("test")) {
+//            InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+//            List < List < InlineKeyboardButton >> rowsInline = new ArrayList < > ();
+//            List < InlineKeyboardButton > rowInline = new ArrayList < > ();
+//            rowInline.add(new InlineKeyboardButton().setText("Open Browser").setUrl("1"));
+//            rowsInline.add(rowInline);
+//            markupInline.setKeyboard(rowsInline);
+//            sendMessage.setReplyMarkup(markupInline);
+//        }
+
+        /*
+        Если пользователь не авторизован
+         */
+//        if (!isAuthorized) {
+//            /*
+//            Объект разметки клавиатуры
+//             */
+//            InlineKeyboardMarkup inlineKeyboardMarkup =new InlineKeyboardMarkup();
+//            /*
+//            Объект кнопки
+//             */
+//            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+//            inlineKeyboardButton.setText("Авторизация");
+//            inlineKeyboardButton.setCallbackData("Введите свой номер телефона");
+//            /*
+//            Добавление кнопки в ряд
+//             */
+//            List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+//            keyboardButtonsRow1.add(inlineKeyboardButton);
+//            /*
+//            Добавление ряда в список рядов
+//             */
+//            List<List<InlineKeyboardButton>> rowList= new ArrayList<>();
+//            rowList.add(keyboardButtonsRow1);
+//            /*
+//            добавление списка рядов в разметку клавиатуры
+//             */
+//            inlineKeyboardMarkup.setKeyboard(rowList);
+//            /*
+//            Добавление разметки клавиатуры к сообщению
+//             */
+//            sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+//        }
+        /*
+
+         */
 
 //        if (isAuthorized==false) {
 //            sendMessage.setText(buttons)
 //        }
-
-
-
+//
+//
+//
+//        if (update.getMessage().getText().equals("/autorize")) {
+//            sendMessage.setText("auth");
+//        } else {
+//            sendMessage.setText(update.getMessage().getText());
+//        }
+//
+//
+//
 //        /**
 //         * Создание клавиатуры, для отображения в чате (кнопки)
 //         */
@@ -113,43 +225,21 @@ public class TelegramBot extends TelegramLongPollingBot {
 //        replyKeyboardMarkup.setKeyboard(keyboard);
 
 
-        /**
-         * Поиск в БД заказа, по введенному номеру телефона
+
+//        update.getUpdateId();
+
+    }
+
+    public void sendMessageFromKafka(Long id) {
+        /*
+        Принимается заказ и по chatId отправляется сообщение об изменении статуса заказа
          */
-        List<Order> orderList= orderService.getOrderByUserPhoneInContacts(update.getMessage().getText());
-
-        update.getUpdateId();
-
-        String id = update.getMessage().getChatId().toString();
-
-
-
-        sendMessage.setChatId(id);
-
-        String responseMessage = "";
-
-//        if (orderList!=null){
-//            responseMessage += "Колличество заказов: " + orderList.size()+"\n";
-//            for (Order order : orderList) {
-//                responseMessage+= "id: " + order.getId()+"\n";
-//                responseMessage+= "data: " +new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.US).format(order.getData())+ "\n";
-//                responseMessage+= "address: " + order.getAddress()+"\n";
-//                responseMessage+= "itemsCost: " + order.getItemsCost()+"\n";
-//                responseMessage+= "trackingNumber: " + order.getTrackingNumber()+"\n";
-//                responseMessage+= "status: " + order.getStatus()+"\n";
-//                responseMessage+= "contacts: " + order.getContacts()+"\n";
-//                responseMessage+= "comment: " + order.getComment()+"\n";
-//            }
-//            sendMessage.setText(responseMessage);
-//        }else {
-//            sendMessage.setText("не найдено заказов по указанному номеру: " +update.getMessage().getText());
-//        }
-
-        sendMessage.setText(update.toString());
-
-        /**
-         Отправка сообщения
-         */
+        SendMessage sendMessage = new SendMessage();
+        Order order = orderService.getOrderById(id);
+        String chatId = order.getContacts().getChatId();
+        String text ="Статус вашего заказа" +order.getId()+"\n";
+        text+="Изменился на "+order.getStatus();
+        sendMessage.setChatId(chatId).setText(text);
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
