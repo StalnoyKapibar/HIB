6465cac1d10b0c3256b371a921c07826b75534bc
Uetal <uetal@me.com>
2020-12-08 09:44:48 +0300

    3,3

--- src/main/java/com/project/telegrambot/TelegramBot.java
+++ src/main/java/com/project/telegrambot/TelegramBot.java
@@ -1,4 +1,4 @@
-package com.project.util;
+package com.project.telegrambot;
 
 import com.project.model.Order;
 import com.project.service.OrderServiceImpl;
@@ -9,6 +9,7 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -70,11 +71,21 @@ public class TelegramBot extends TelegramLongPollingBot {
         replyKeyboardMarkup.setOneTimeKeyboard(false);
 
         /**
-         * Список строк для клавиатуры
+         * Список строк для клавиатуры,
+         * Создание строк клавиатуры,
+         * Добавление в них кнопок
+         * Добавление строк в список
+         * Установить список строк клавиатуре
          */
         List<KeyboardRow> keyboard = new ArrayList<>();
         KeyboardRow keyboardRow1 = new KeyboardRow();
         KeyboardRow keyboardRow2 = new KeyboardRow();
+        keyboardRow1.add(new KeyboardButton("Кнопка 1 строка 1"));
+        keyboardRow1.add(new KeyboardButton("Кнопка 2 строка 1"));
+        keyboardRow2.add(new KeyboardButton("Кнопка 1 строка 2"));
+        keyboard.add(keyboardRow1);
+        keyboard.add(keyboardRow2);
+        replyKeyboardMarkup.setKeyboard(keyboard);
 
 
         /**
@@ -92,22 +103,24 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         String responseMessage = "";
 
-        if (orderList!=null){
-            responseMessage += "Колличество заказов: " + orderList.size()+"\n";
-            for (Order order : orderList) {
-                responseMessage+= "id: " + order.getId()+"\n";
-                responseMessage+= "data: " +new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.US).format(order.getData())+ "\n";
-                responseMessage+= "address: " + order.getAddress()+"\n";
-                responseMessage+= "itemsCost: " + order.getItemsCost()+"\n";
-                responseMessage+= "trackingNumber: " + order.getTrackingNumber()+"\n";
-                responseMessage+= "status: " + order.getStatus()+"\n";
-                responseMessage+= "contacts: " + order.getContacts()+"\n";
-                responseMessage+= "comment: " + order.getComment()+"\n";
-            }
-            sendMessage.setText(responseMessage);
-        }else {
-            sendMessage.setText("не найдено заказов по указанному номеру: " +update.getMessage().getText());
-        }
+//        if (orderList!=null){
+//            responseMessage += "Колличество заказов: " + orderList.size()+"\n";
+//            for (Order order : orderList) {
+//                responseMessage+= "id: " + order.getId()+"\n";
+//                responseMessage+= "data: " +new SimpleDateFormat("dd.MM.yyyy HH:mm:ss", Locale.US).format(order.getData())+ "\n";
+//                responseMessage+= "address: " + order.getAddress()+"\n";
+//                responseMessage+= "itemsCost: " + order.getItemsCost()+"\n";
+//                responseMessage+= "trackingNumber: " + order.getTrackingNumber()+"\n";
+//                responseMessage+= "status: " + order.getStatus()+"\n";
+//                responseMessage+= "contacts: " + order.getContacts()+"\n";
+//                responseMessage+= "comment: " + order.getComment()+"\n";
+//            }
+//            sendMessage.setText(responseMessage);
+//        }else {
+//            sendMessage.setText("не найдено заказов по указанному номеру: " +update.getMessage().getText());
+//        }
+
+        sendMessage.setText(update.toString());
 
         /**
          Отправка сообщения
